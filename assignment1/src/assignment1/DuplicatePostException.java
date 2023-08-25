3c61ea40bd513d923da413281de9fb16602b72de
Scott <Scott@DESKTOP-QIFF3K0>
2023-08-25 10:04:08 +1000

    Generated serial ID

--- assignment1/src/assignment1/DuplicatePostException.java
+++ assignment1/src/assignment1/DuplicatePostException.java
@@ -1,6 +1,11 @@
 package assignment1;
 
 public class DuplicatePostException extends Exception {
+	/**
+	 * Generated serial ID
+	 */
+	private static final long serialVersionUID = -7884667136728976122L;
+
 	public DuplicatePostException() {
 	}
 
